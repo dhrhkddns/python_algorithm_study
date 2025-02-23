92d72087c4ef395ef163610ce41efc96d0b1bd39
ohdaniel7 <59607551+dhrhkddns@users.noreply.github.com>
2025-02-23 21:30:20 +0900

    Add files via upload

---
+++ stackie.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Stack:
+    def __init__(self):
+        self.top = None
+    def push(self, data):
+        if self.top is None:
+            self.top = Node(data)
+        else:
+            node = Node(data)
+            node.next = self.top
+            self.top = node
+    def pop(self):
+        if self.top is None:
+            return None
+        node = self.top
+        self.top = self.top.next
+        return node.data
+    def peek(self):
+        if self.top is None:
+            return None
+        return self.top.data
+    def is_empty(self):
+        return self.top is None
+
+if __name__ == "__main__":
+    s = Stack()
+
+    for i in range(3):
+        s.push(chr(ord("A") + i))
+        print(f"Push data = {chr(ord("A") + i)}")
+    print()
+
+    while not s.is_empty():
+        print(f"Pop data = {s.pop()}")
+
+    print()
+    print(f"Peek data after getting empty: {s.peek()}")
